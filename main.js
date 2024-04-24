50319bc204a4c5b362fe08b9b5c5714a07f60f0c
VitaSucc <vitalijus.kirciginas@gmail.com>
2024-04-24 10:44:22 +0200

    Showcase followed up

---
+++ main.js
@@ -0,0 +1,18 @@
+//Define a variable
+let greeting = "Yo sup Kodeloftet"
+//Give it value
+    greeting = "A value"
+//Do something with the variable
+    console.log(greeting)
+//CHange the value to variable (Give it a new value)
+    greeting = "A different value"
+    console.log(greeting)
+//Define constants
+const constantVariable = "I am constant"
+//The next line crashes the program
+//constantVariable = "This does not work"
+
+/**
+ * Many lines
+ * of comments
+ */
